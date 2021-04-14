5367ca3bb5c3fd80a3d9f6adf0e2763d06ef1c40
Troels Bjørnskov <troels@bjoernskov.org>
2021-04-14 16:12:57 +0200

    Add find-files-goto.py

---
+++ find-files-goto.py
@@ -0,0 +1,41 @@
+from os import path
+import sublime
+import sublime_plugin
+import re
+
+class FindInFilesGotoCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        view = self.view
+        if view.name() == "Find Results":
+            line_no = self.get_line_no()
+            file_name = self.get_file()
+            if line_no is not None and file_name is not None:
+                caretpos = view.sel()[0].begin()
+                (_,col) = view.rowcol(caretpos)
+                file_loc = "%s:%s:%s" % (file_name, line_no, col -6)
+                view.window().open_file(file_loc, sublime.ENCODED_POSITION)
+            elif file_name is not None:
+                view.window().open_file(file_name)
+
+    def get_line_no(self):
+        view = self.view
+        if len(view.sel()) == 1:
+            line_text = view.substr(view.line(view.sel()[0]))
+            match = re.match(r"\s*(\d+).+", line_text)
+            if match:
+                return match.group(1)
+        return None
+
+    def get_file(self):
+        view = self.view
+        if len(view.sel()) == 1:
+            line = view.line(view.sel()[0])
+            while line.begin() > 0:
+                line_text = view.substr(line)
+                match = re.match(r"(.+):$", line_text)
+                if match:
+                    if path.exists(match.group(1)):
+                        return match.group(1)
+                line = view.line(line.begin() - 1)
+        return None
