9e0e908c2e6ad6076a67d61ad6a2e1ebf251c26f
Troels Bjørnskov <troels@bjoernskov.org>
2021-05-12 09:29:27 +0200

    Remove SIGUSR again

--- save_and_close_git_commit.py
+++ save_and_close_git_commit.py
@@ -7,4 +7,3 @@ class SaveAndCloseGitCommitCommand(sublime_plugin.WindowCommand):
         sublime.active_window().run_command('save')
         sublime.active_window().run_command('close')
         subprocess.Popen(['swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'grow', 'width', '2px,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '340px,', 'move', 'down', '1px']).wait()
-        subprocess.Popen(['/usr/bin/pkill', 'zsh', '--signal=USR1']).wait()
