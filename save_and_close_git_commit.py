576bc8d48cc2a014701b7704410d6cdb7fdf4723
Troels Bjørnskov <troels@bjoernskov.org>
2021-05-12 09:22:45 +0200

    Redo sway scratchpad command

--- save_and_close_git_commit.py
+++ save_and_close_git_commit.py
@@ -6,4 +6,4 @@ class SaveAndCloseGitCommitCommand(sublime_plugin.WindowCommand):
     def run(self):
         sublime.active_window().run_command('save')
         sublime.active_window().run_command('close')
-        subprocess.Popen([ 'swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '1502px', 'height', '1002px,', 'resize', 'shrink', 'up', '650px']).wait()
+        subprocess.Popen([ 'swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'grow', 'width', '2px,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '340px,', 'move', 'down', '1px']).wait()
