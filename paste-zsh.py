bc36d64f9f529468f93434d76c71d3d8efb44bb3
Troels Bjørnskov <troels@bjoernskov.org>
2021-06-09 20:57:34 +0200

    Adjust scratchpad height

--- paste-zsh.py
+++ paste-zsh.py
@@ -4,5 +4,5 @@ import subprocess
 class PasteZshCommand(sublime_plugin.WindowCommand):
     def run(self):
         subprocess.Popen(['/usr/bin/pkill', 'zsh', '--signal=USR2']).wait()
-        subprocess.Popen(['swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'grow', 'width', '2px,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '340px,', 'move', 'down', '1px']).wait()
+        subprocess.Popen(['swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'grow', 'width', '2px,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '337px,', 'move', 'down', '1px']).wait()
 
