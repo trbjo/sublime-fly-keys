c14732cb2f4844dbf7e5fcd4d6e13de8402b6400
Troels Bjørnskov <troels@bjoernskov.org>
2021-07-29 12:28:01 +0200

    Lots of changes

--- save_and_close_git_commit.py
+++ save_and_close_git_commit.py
@@ -1,9 +1,102 @@
 import sublime
 import sublime_plugin
-import subprocess
+from subprocess import check_output, Popen
+from collections import OrderedDict
+import os
 
-class SaveAndCloseGitCommitCommand(sublime_plugin.WindowCommand):
+
+class PromptGitCommand(sublime_plugin.WindowCommand):
+    last_selected = 0
+
+    def is_enabled(self):
+        view = self.window.active_view()
+        if view:
+            scope = view.scope_name(view.sel()[0].begin()).split(' ')[0]
+            if scope == 'text.git.commit':
+                return True
+        return False
+
     def run(self):
+        git_actions_pretty = [
+        '1: Commit and Push',
+        '2: Commit only',
+        '3: Commit, Rebase and Push'
+        ]
+
+        self.window.show_quick_panel(
+            git_actions_pretty,
+            self.transform,
+            selected_index=self.last_selected)
+
+    def transform(self, i):
+        print(i)
+        if i == -1:
+            return
+        self.last_selected = i
+        git_action = {
+        '1': ['git', 'push'],
+        '2': [''],
+        '3': ['git', 'pull', '--rebase', '&&', 'git', 'push']
+        }
         sublime.active_window().run_command('save')
         sublime.active_window().run_command('close')
-        subprocess.Popen(['swaymsg', '[app_id="^PopUp$"]', 'scratchpad', 'show,', 'fullscreen', 'disable,', 'move', 'position', 'center,', 'resize', 'set', 'width', '100ppt', 'height', '100ppt,', 'resize', 'grow', 'width', '2px,', 'resize', 'shrink', 'up', '1100px,', 'resize', 'grow', 'up', '340px,', 'move', 'down', '1px']).wait()
+        pwd = self.window.active_view().file_name().rsplit('/', 1)[0]
+        Popen(git_action[i], cwd=pwd, shell=False)
+
+
+
+class GitResetHead(sublime_plugin.WindowCommand):
+    def run(self, edit=None):
+        self.run_command(['git', 'reset', 'HEAD', self.get_file_name()])
+
+
+
+class GitCommitNewCommand(sublime_plugin.WindowCommand):
+    global git_dirs
+    global non_git_dirs
+    git_dirs = []
+    non_git_dirs = []
+
+    def run(self):
+        pwd = self.window.active_view().file_name().rsplit('/', 1)[0]
+        try:
+            Popen(['git', 'commit', '-v'], cwd=pwd)
+        except:
+            sublime.status_message('Nothing to commit')
+        # if sp.returncode != 0:
+        #     output = sp.communicate()[0]
+        #     last_line = output.strip().rsplit('\n', 1)[-1].capitalize()
+        #     print(output)
+        #     sublime.status_message(last_line)
+
+
+    def is_enabled(self):
+        pwd = self.window.active_view().file_name().rsplit('/', 1)[0]
+        if pwd is None:
+            return False
+
+        for repo in git_dirs:
+            if repo in pwd:
+                return True
+        for directory in non_git_dirs:
+            if directory in pwd:
+                return False
+
+        result = self.is_git_dir(pwd)
+        if result:
+            git_dirs.append(result)
+            return True
+        else:
+            non_git_dirs.append(pwd)
+            return False
+
+
+    def is_git_dir(self, pwd) -> str:
+        while pwd:
+            if os.path.exists(os.path.join(pwd, '.git')):
+                return pwd
+            parent = os.path.realpath(os.path.join(pwd, os.path.pardir))
+            if parent == pwd:
+                # /.. == /
+                return ''
+            pwd = parent
