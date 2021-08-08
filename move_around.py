cf7d7c9ced27f850a43152c5ca4926f63955cf75
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-08 07:40:11 +0200

    Only track first and last selection, respectively, when navigating by paragraph

--- move_around.py
+++ move_around.py
@@ -1,19 +1,18 @@
 import sublime
 import sublime_plugin
-import bisect
+
 import re
 
 class NavigateByParagraphForwardCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         buf = self.view
-        for region in buf.sel():
-            _, next_res = buf.find('\n\n\\s*', region.begin())
-            if next_res == -1:
-                next_res = buf.size()
-            reg = sublime.Region(next_res, next_res)
-            buf.sel().subtract(region)
-            buf.sel().add(reg)
-
+        region = buf.sel()[-1]
+        _, next_res = buf.find(r'\n\n\s*', region.begin())
+        if next_res == -1:
+            next_res = buf.size()
+        reg = sublime.Region(next_res, next_res)
+        buf.sel().clear()
+        buf.sel().add(reg)
         buf.show(reg, False)
 
 
@@ -23,11 +22,11 @@ class NavigateByParagraphBackwardCommand(sublime_plugin.TextCommand):
         region = buf.sel()[0]
         buffer_rev = buf.substr(sublime.Region(-1, region.begin() - 1))[::-1]
         for m in re.finditer(r'\S *\n\n', buffer_rev):
-            end = region.begin() - m.start()
+            next_res = region.begin() - m.start() - 2
             break
         else:
-            end = -1
-        reg = sublime.Region(end - 2, end - 2)
+            next_res = -1
+        reg = sublime.Region(next_res, next_res)
         buf.sel().clear()
         buf.sel().add(reg)
         buf.show(reg, False)
