09e78f29d51a2fee0cdfc334826284bca5133386
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-08 06:52:56 +0200

    Fix typo buffer_prev -> buffer_rev

--- extended_expand_to_paragraph.py
+++ extended_expand_to_paragraph.py
@@ -3,15 +3,14 @@ import sublime_plugin
 
 import re
 
-
 class ExtendedExpandSelectionToParagraphCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         buf = self.view
-        buffer_prev = None
+        buffer_rev = None
         for region in buf.sel():
             if region.empty():
                 # selection is empty, we need to look behind:
-                if buffer_prev is None:
+                if buffer_rev is None:
                     buffer_rev = buf.substr(sublime.Region(-1, buf.sel()[-1].begin() + 1))[::-1]
                 for m in re.finditer(r'\S *\n\n', buffer_rev):
                     beginning = region.begin() - m.start()
@@ -20,10 +19,12 @@ class ExtendedExpandSelectionToParagraphCommand(sublime_plugin.TextCommand):
                     beginning = -1
             else:
                 beginning = region.begin()
+
             _, next_res = buf.find(r'\S\n\n', region.end() - 1)
             if next_res == -1:
-                next_res = buf.size()
-            reg = sublime.Region(beginning, next_res -1)
+                next_res = buf.size() + 1
+
+            reg = sublime.Region(beginning, next_res - 1)
             buf.sel().add(reg)
         try:
             buf.show(reg, False)
