ca7ceeb737fdc7f62abffadaffbcef6ddfa3fb5a
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-09 23:17:17 +0200

    Convert multiple_cursors_from_selection from macro to python

--- multiple_cursors_from_selection.py
+++ multiple_cursors_from_selection.py
@@ -1,15 +1,20 @@
 import sublime
 import sublime_plugin
 
+import re
+
 class MultipleCursorsFromSelectionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         buf = self.view
-        lines = []
+        reg_list = []
         for region in buf.sel():
-            line_begin, _ = buf.rowcol(region.begin())
-            line_end, _ = buf.rowcol(region.end())
-            for line in range(line_end - line_begin):
-                line_in_point, _ = sublime.Region(buf.text_point(line_begin + line, 0),0)
-                lines.append((buf.find(r'\S', line_in_point).begin()))
+            reg_begin = region.begin() - 1
+            reg_end = region.end() - 1
+            buffer = buf.substr(sublime.Region(reg_begin, reg_end))
+            if reg_begin > 1:
+                reg_begin -= 1
+            else:
+                reg_list.append(-1)
+            reg_list += [m.end() + reg_begin for m in re.finditer(r'\n *\S', buffer)]
         buf.sel().clear()
-        buf.sel().add_all(lines)
+        buf.sel().add_all([sublime.Region(i) for i in reg_list])
