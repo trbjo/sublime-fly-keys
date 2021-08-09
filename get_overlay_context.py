cfbeda25f954689273c9b22ee045950d7e00a477
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-09 21:35:07 +0200

    Goto soft bol in goto line plugin

--- get_overlay_context.py
+++ get_overlay_context.py
@@ -5,17 +5,19 @@ prev_buf_id = 0
 pos_begin = 0
 allow_extend = False
 ought_to_extend = False
+should_change_to_bol = False
 
 class SampleListener(sublime_plugin.EventListener):
-
     def on_query_context(self, view, key, operator, operand, match_all):
         global allow_extend
         global ought_to_extend
+        global should_change_to_bol
         if key in ("goto_anything:input"):
             lhs = view.element() == "goto_anything:input"
             if view.element() == "goto_anything:input":
                 if ought_to_extend == True:
                     allow_extend = True
+                should_change_to_bol = True
             else:
                 allow_extend = False
             rhs = bool(operand)
@@ -27,14 +29,22 @@ class SampleListener(sublime_plugin.EventListener):
         global prev_buf_id
         global pos_begin
         global allow_extend
-        v = sublime.active_window().active_view()
-        end = v.full_line(v.sel()[0].end()).end()
+        global should_change_to_bol
         if allow_extend == True:
             allow_extend = False
+            v = sublime.active_window().active_view()
             if prev_buf_id == v.id():
+                end = v.full_line(v.sel()[0].end()).end()
                 v.sel().add(sublime.Region(pos_begin,end))
+        elif should_change_to_bol:
+            v = sublime.active_window().active_view()
+            end = v.full_line(v.sel()[0].end()).end()
+            should_change_to_bol = False
+            next_res, next_res_end = v.find(r'\S|^$|^\s+$', v.sel()[0].end())
+            v.sel().clear()
+            v.sel().add(sublime.Region(next_res,next_res))
 
-    def on_deactivated(self, view):
+    def on_deactivated_async(self, view):
         global prev_buf_id
         global pos_begin
         global ought_to_extend
