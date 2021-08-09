3b39385126bfd074bdfe632e146a47468cc4e59e
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-09 21:36:01 +0200

    Fix bug and selected newline to be at the end of the selection instead of beginning

--- extended_expand_to_paragraph.py
+++ extended_expand_to_paragraph.py
@@ -1,9 +1,7 @@
 import sublime
 import sublime_plugin
-import bisect
-import itertools
 
-import re
+import bisect
 
 class ExtendedExpandSelectionToParagraphForwardCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -14,38 +12,46 @@ class ExtendedExpandSelectionToParagraphForwardCommand(sublime_plugin.TextComman
 
             if region.empty():
                 if whitespaces is None:
-                    whitespaces = buf.find_all(r'\n\n *')
-                    first, last = zip(*whitespaces)
+                    whitespaces = buf.find_all(r'\n\n *\S')
+                    try:
+                        first, last = zip(*whitespaces)
+                    except ValueError:
+                        first, last = [2], [0]
 
-                bisect_end = bisect.bisect(first, region.end() -1)
+                bisect_end = bisect.bisect(first, region.end() - 2)
                 if bisect_end >= len(whitespaces):
                     sel_end = buf.size() + 1
                 else:
                     sel_end, _ = whitespaces[bisect_end]
-                    sel_end += 2
+                    sel_end += 3
 
-                bisect_begin = bisect.bisect(whitespaces, region) - 1
-                if bisect_begin == -1:
-                    beginning = -1
+                bisect_begin = bisect.bisect(first, region.begin() -2 )
+                if bisect_begin == 0:
+                    sel_begin = -1
                 else:
-                    beginning, _ = whitespaces[bisect_begin]
-                    beginning += 1
+                    sel_begin, _ = whitespaces[bisect_begin -1 ]
+                    sel_begin += 2
 
             else:
 
-                beginning = region.begin()
-                if whitespaces is None:
-                    _, sel_end = buf.find(r'\S\n\n', region.end() - 1)
-                    if sel_end == -1:
-                        sel_end = buf.size() + 1
-                else:
-                    if bisect_end >= len(whitespaces) - 1:
-                        sel_end = buf.size() + 1
-                    else:
-                        sel_end, _ = whitespaces[bisect_end + 1]
-                        sel_end += 2
+                sel_begin = region.begin()
 
-            regs_dict[beginning] = sel_end
+                if whitespaces is None:
+                    sel_end, _ = buf.find(r'\n\n *\S', region.end() + 2)
+                    if sel_end == -1:
+                        sel_end = buf.size()
+
+                else:
+
+                    bisect_end = bisect.bisect(first, region.end() - 2)
+                    if bisect_end >= len(whitespaces):
+                        sel_end = buf.size()
+                    else:
+                        sel_end, _ = whitespaces[bisect_end]
+
+                sel_end += 3
+
+            regs_dict[sel_begin] = sel_end
 
         buf.sel().add_all([sublime.Region(begin,end -1) for begin,end in regs_dict.items()])
         buf.show(buf.sel()[-1], False)
