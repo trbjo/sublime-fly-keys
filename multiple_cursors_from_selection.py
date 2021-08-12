dd640464b2a4c861dbd5a4799d91a4495eaeb43b
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-12 16:00:32 +0200

    Change regex

--- multiple_cursors_from_selection.py
+++ multiple_cursors_from_selection.py
@@ -13,6 +13,6 @@ class MultipleCursorsFromSelectionCommand(sublime_plugin.TextCommand):
             if reg_begin <= 1:
                 reg_begin += 1
                 reg_list.append(-2)
-            reg_list += [sublime.Region(m.start() + reg_begin) for m in re.finditer(r'\S.+\n', buffer)]
+            reg_list += [sublime.Region(m.start() + reg_begin) for m in re.finditer(r'\S.*\n', buffer)]
         buf.sel().clear()
         buf.sel().add_all(reg_list)
