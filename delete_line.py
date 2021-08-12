79e6a9b3cad5be4dd98f5060876b47ace14b87c0
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-12 16:00:55 +0200

    Take EOF into account in delete_line

--- delete_line.py
+++ delete_line.py
@@ -1,15 +1,24 @@
 import sublime
 import sublime_plugin
 
+
+# hej
+
 class DeleteSmartCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         buf = self.view
         for region in reversed(buf.sel()):
-            begin_line, _ = buf.rowcol(region.begin())
-            end_line, col = buf.rowcol(region.end())
-            if col != 0:
-                end_line += 1
-            reg_beg = buf.text_point(begin_line, 0)
-            reg_end = buf.text_point(end_line, 0) - 1
-            reg = sublime.Region(reg_beg, reg_end + 1)
+            if region.empty():
+                if region.a == buf.size():
+                    reg = buf.full_line(region.begin() -1)
+                else:
+                    reg = buf.full_line(region.begin())
+            else:
+                begin_line, _ = buf.rowcol(region.begin())
+                end_line, col = buf.rowcol(region.end())
+                if col != 0:
+                    end_line += 1
+                reg_beg = buf.text_point(begin_line, 0)
+                reg_end = buf.text_point(end_line, 0) - 1
+                reg = sublime.Region(reg_beg, reg_end + 1)
             buf.erase(edit, reg)
