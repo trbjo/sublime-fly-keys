3527fbf3e2c94c184b3e497acee7660d1c73639a
Troels Bjørnskov <troels@bjoernskov.org>
2021-08-15 12:43:24 +0200

    Refactor insert_before_command and move to insert_mode_command_mode

--- insert_mode_command_mode.py
+++ insert_mode_command_mode.py
@@ -10,9 +10,31 @@ class InsertModeCommand(sublime_plugin.TextCommand):
         buf.settings().set(key="block_caret", value=False)
         buf.settings().set(key="command_mode", value=False)
 
+
 class CommandModeCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, _):
         buf = self.view
         buf.settings().set(key="block_caret", value=True)
         buf.settings().set(key="command_mode", value=True)
+
+
+class InsertBeforeOrAfterCommand(sublime_plugin.TextCommand):
+    def run(self, _, after=False):
+        buf = self.view
+        selections = buf.sel()
+        for region in selections:
+
+            if region.empty():
+                continue
+
+            if after == True:
+                reg = region.end() + 1
+            else:
+                reg = region.begin() - 1
+
+            selections.subtract(region)
+            selections.add(reg)
+
+        buf.settings().set(key="block_caret", value=False)
+        buf.settings().set(key="command_mode", value=False)
 
