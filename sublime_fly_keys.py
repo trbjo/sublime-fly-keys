cc805f5a33421973ae147984272b1d1240c8f525
Troels Bjørnskov <troels@bjoernskov.org>
2022-03-18 11:39:25 +0100

    Add ExpandSelectionToStringCommand instead of smart

--- sublime_fly_keys.py
+++ sublime_fly_keys.py
@@ -207,6 +207,32 @@ class ExpandSelectionToSentenceCommand(sublime_plugin.TextCommand):
             view.sel().add(sublime.Region(thisRegionBegin, thisRegionBegin))
 
 
+class ExpandSelectionToStringCommand(sublime_plugin.TextCommand):
+    # TODO: Add foward command to go forward and backward selction of sentences
+    def run(self, _):
+        view = self.view
+        # whitespace = '\t\n\x0b\x0c\r ' # Equivalent to string.whitespace
+        oldSelRegions = list(view.sel())
+        view.sel().clear()
+        for thisregion in oldSelRegions:
+            thisRegionBegin = thisregion.begin() - 1
+            while ((view.substr(thisRegionBegin) not in '"') and (thisRegionBegin >= 0)):
+                thisRegionBegin -= 1
+            thisRegionBegin += 1
+        while((view.substr(thisRegionBegin) in string.whitespace) and (thisRegionBegin < view.size())):
+            thisRegionBegin += 1
+
+        thisRegionEnd = thisregion.end()
+        while((view.substr(thisRegionEnd) not in '"') and (thisRegionEnd < view.size())):
+            thisRegionEnd += 1
+        thisRegionEnd -= 1
+
+        if(thisRegionBegin != thisRegionEnd):
+            view.sel().add(sublime.Region(thisRegionBegin, thisRegionEnd+1))
+        else:
+            view.sel().add(sublime.Region(thisRegionBegin, thisRegionBegin))
+
+
 class SetReadOnly(sublime_plugin.EventListener):
     def on_new_async(self, view):
         if view.name() == 'Find Results':
