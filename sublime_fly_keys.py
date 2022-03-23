4be783eae241974228794a56365e27666db88e6d
Troels Bjørnskov <troels@bjoernskov.org>
2022-03-23 18:09:07 +0100

    Improve matching algorithm in ToggleTrueFalseCommand

--- sublime_fly_keys.py
+++ sublime_fly_keys.py
@@ -43,8 +43,24 @@ class ToggleTrueFalseCommand(sublime_plugin.TextCommand):
         for region in reversed(buf.sel()):
             if region.empty():
                 linestr = self.view.substr(sublime.Region(region.a - 6, region.a + 6))
-                g = re.search(r'((F|f)alse|(T|t)rue)', linestr)
-                if g is None:
+                g = [(m.start(), m.end(), m.groups()[0]) for m in re.finditer(r'((F|f)alse|(T|t)rue)', linestr)]
+                # if more than one match was found, we take the one that is nearest
+                # the caret
+                if len(g) > 2:
+                    return
+                elif len(g) == 2:
+                    if abs(6-g[1][0]) > abs(6-g[0][1]):
+                        myval = 0
+                    else:
+                        myval = 1
+                    begin = g[myval][0] + region.begin() - 6
+                    end = g[myval][1] + region.begin() - 6
+                    mybool = g[myval][2]
+                elif len(g) == 1:
+                    begin = g[0][0] + region.begin() - 6
+                    end = g[0][1] + region.begin() - 6
+                    mybool = g[0][2]
+                elif len(g) == 0:
                     lr = self.view.line(region.begin())
                     linestr = self.view.substr(sublime.Region(lr.a, lr.b))
                     g = re.search(r'((F|f)alse|(T|t)rue)', linestr)
@@ -52,14 +68,9 @@ class ToggleTrueFalseCommand(sublime_plugin.TextCommand):
                         return
                     begin = g.span()[0] + lr.a
                     end = g.span()[1] + lr.a
-
-                else:
-                    begin = g.span()[0] + region.begin() - 6
-                    end = g.span()[1] + region.begin() - 6
-
+                    mybool = g.group(0)
                 myregion=sublime.Region(begin, end)
                 buf.sel().subtract(region)
-                mybool=g.group(0)
                 myopposite=true_false_dict[mybool]
                 buf.replace(edit, myregion, myopposite)
                 buf.sel().add(begin)
