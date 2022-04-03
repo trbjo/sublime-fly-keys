aa33f2d1fac532943ea3f389ec37522a89f2d3d7
Troels Bjørnskov <troels@bjoernskov.org>
2022-04-03 15:56:59 +0200

    Fix Sneak

--- sublime_fly_keys.py
+++ sublime_fly_keys.py
@@ -1,8 +1,8 @@
-from sublime import Edit, View, Region, Selection, get_clipboard, set_clipboard, active_window
+from sublime import LAYOUT_INLINE, LITERAL, Edit, View, Region, Selection, get_clipboard, set_clipboard, active_window
 import sublime
 import sublime_plugin
 
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Union
 from os import path
 import bisect
 import datetime
@@ -578,7 +578,7 @@ def build_or_rebuild_ws_for_view(view: View, immediate: bool):
     if (datetime.datetime.now() - timeout).total_seconds() > 2 or immediate == True:
         interesting_regions[view] = dict()
         try:
-            whitespaces: List[Region] = view.find_all(r'\n *\n *\S')
+            whitespaces: List[Region] = view.find_all(r'\n[\t ]*\n[\t ]*\S')
             size = view.size() + 1
             first, last = zip(*[(-2, -1)] + [(first, last -1) for first, last in whitespaces] + [(size, size)])
             interesting_regions[view]['first'] = first
@@ -616,7 +616,7 @@ class NavigateByParagraphForwardCommand(sublime_plugin.TextCommand):
         reg = Region(sel_end)
         buf.sel().clear()
         buf.sel().add(reg)
-        buf.show(reg, False)
+        buf.show(reg, True)
 
 
 class NavigateByParagraphBackwardCommand(sublime_plugin.TextCommand):
@@ -633,8 +633,8 @@ class NavigateByParagraphBackwardCommand(sublime_plugin.TextCommand):
         reg = Region(sel_end)
         buf.sel().clear()
         buf.sel().add(reg)
-        buf.show(reg, False)
 
+        buf.show(reg, True)
 
 class ExtendedExpandSelectionToParagraphForwardCommand(sublime_plugin.TextCommand):
     def run(self, _) -> None:
@@ -722,80 +722,93 @@ class ExtendedExpandSelectionToParagraphBackwardCommand(sublime_plugin.TextComma
 
 # tuple of (char: str, forward: bool, extend: bool)
 char_forward_tuple: Tuple[str, bool, bool] = ('', True, False)
+matches : List[Region] = []
 class FindNextCharacterBaseCommand(sublime_plugin.TextCommand):
     def find_next(self, forward: bool, char: str, pt: int) -> int:
-        char = char[::-1] if not forward else char
-        lr = self.view.line(pt)
         if forward:
-            while lr.b <= self.view.size():
-                linestr = self.view.substr(Region(pt, lr.b))
-                idx = linestr.find(char, 1)
-                if idx >= 0:
-                    return pt + idx
-                else:
-                    lr = self.view.line(lr.b + 1)
+            return self.view.find(char, pt + 1, LITERAL).a
         else:
-            while lr.a >= 0:
-                linestr = self.view.substr(Region(lr.a, pt))[::-1]
-                idx = linestr.find(char)
-                if idx >= 0:
-                    return pt - idx - 1
-                else:
-                    lr = self.view.line(lr.a - 1)
-        return pt
+            char = char[::-1]
+            mybuf = self.view.substr(Region(0, pt -1))
+            try:
+                return pt - mybuf[::-1].index(char) -2
+            except ValueError:
+                return -1
 
-    max_res: int = 3
-    def get_next_chars(self, forward: bool, search_string: str):
-        if len(self.view.sel()) != 1:
-            return
-        matches: List[Region] = []
-        old_pt = self.view.sel()[0].b
-        while len(matches) < self.max_res:
-            new_pt = self.find_next(forward, search_string, old_pt)
-            if new_pt == old_pt:
-                break
-            pt = new_pt if forward else new_pt -1
-            matches.append(Region(pt, pt + len(search_string)))
-            old_pt = new_pt
-        if len(matches) > 0:
-            self.view.add_regions("Sneak", matches, "special_line", flags=sublime.DRAW_NO_OUTLINE)
-        return
 
-    def move_or_expand_selections(self, character: str, forward: bool, extend: bool) -> None:
+    myhtml="""<body id="my-plugin-feature"><style>div.matcher{{padding: 0 2px 0 1px; margin: 0; border-radius:2px;background-color:{background};color:{color};}}</style><div class="matcher">{counter}</div></body>"""
+    charlist = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    def execute(self, forward: bool, search_string: str, extend: bool) -> None:
         buf = self.view
-        for region in buf.sel():
-            pt = self.find_next(forward, character, region.b)
-            if pt == region.b:
+        sels: Selection = buf.sel()
+        regs_to_add: List[Union[Region,int]] = []
+        regs_to_subtract: List[Region] = []
+        offset = 1 if not forward and len(search_string) == 1 else 0
+        for region in sels:
+            pt = self.find_next(forward, search_string, region.b)
+            if pt == -1:
                 return
             if region.a == region.b:
                 if extend:
-                    tp = pt + 2 if forward else pt -1
-                    buf.sel().add(sublime.Region(region.a, tp))
+                    tp = pt + len(search_string) if forward else pt -1
+                    regs_to_add.append(sublime.Region(region.a, tp))
                 else:
                     tp = pt if forward else pt -1
-                    buf.sel().subtract(region)
-                    buf.sel().add(tp)
+                    regs_to_subtract.append(region)
+                    regs_to_add.append(tp + offset)
                 # normal sel
             elif region.a < region.b:
                 if forward:
-                    buf.sel().add(Region(region.b -1, pt + 2))
+                    regs_to_add.append(Region(region.b -1, pt + len(search_string)))
                 else:
                     if pt < region.a:
-                        buf.sel().subtract(Region(region.b, region.a))
-                        buf.sel().add(Region(region.a))
+                        regs_to_subtract.append(Region(region.b, region.a))
+                        regs_to_add.append(Region(region.a))
                     else:
-                        buf.sel().subtract(Region(region.b, pt+1))
+                        regs_to_subtract.append(Region(region.b, pt+1))
                 # reverse sel
             elif region.a > region.b:
                 if forward:
                     if pt > region.a:
-                        buf.sel().subtract(Region(region.a, region.b))
-                        buf.sel().add(Region(region.a))
+                        regs_to_subtract.append(Region(region.a, region.b))
+                        regs_to_add.append(Region(region.a))
                     else:
-                        buf.sel().subtract(Region(region.b, pt))
+                        regs_to_subtract.append(Region(region.b, pt))
                 else:
-                    buf.sel().add(Region(region.a, pt -1))
+                    regs_to_add.append(Region(region.a, pt -1 + offset))
+
+        for reg in regs_to_subtract:
+            sels.subtract(reg)
+        sels.add_all(regs_to_add)
         buf.show(buf.sel()[-1], True)
+
+        # now we look for highlights
+        offset = offset-1 if not forward else offset
+        max_res: int = 1 if len(sels) > 1 else 10
+        global matches
+        matches = []
+        for region in sels:
+            pt = region.b
+            for _ in range(max_res):
+                new_pt = self.find_next(forward, search_string, pt)
+                if new_pt == pt or new_pt == -1:
+                    break
+                pt = new_pt + offset
+                matches.append(Region(pt, pt + len(search_string)))
+                pt = new_pt
+        if not matches:
+            return
+        # we use phantoms if we have one match, regions, if more
+        if len(sels) > 1:
+            for match in enumerate(matches):
+                self.view.add_regions("Sneak", matches, "accent", flags=sublime.DRAW_NO_OUTLINE)
+            return
+
+        bg_color = buf.style()["accent"]
+        fg_color = buf.style()["background"]
+
+        for i,match in enumerate(matches):
+            self.view.add_phantom("Sneak", region=match, content=self.myhtml.format(counter=self.charlist[i], background=bg_color, color=fg_color), layout=LAYOUT_INLINE)
         return
 
 class StoreCharacterCommand(FindNextCharacterBaseCommand):
@@ -806,20 +819,49 @@ class StoreCharacterCommand(FindNextCharacterBaseCommand):
 
 class RepeatFindNextCharacterCommand(FindNextCharacterBaseCommand):
     def run(self, _, **kwargs: bool) -> None:
-        self.view.settings().set(key="has_stored_char", value=True)
-        global char_forward_tuple
+        search_string, forward, extend = char_forward_tuple
+        if not search_string:
+            return
         if bool(kwargs):
             forward = kwargs['forward']
-            search_string, _, _ = char_forward_tuple
-        else:
-            search_string, forward, _ = char_forward_tuple
+        self.view.settings().set(key="has_stored_search", value=True)
+        self.execute(forward, search_string, extend)
 
-        self.move_or_expand_selections(search_string, forward, False)
-        self.get_next_chars(forward, search_string)
+class GoToNthMatchCommand(FindNextCharacterBaseCommand):
+    def run(self, _, **kwargs: int) -> None:
+        if bool(kwargs):
+            number: int = kwargs['number']
+        else:
+            return
+        buf = self.view
+        sels: Selection = buf.sel()
+        region = sels[0]
+        if len(sels) != 1:
+            return
+        _, _, extend = char_forward_tuple
+        if region.a != region.b:
+            extend = True
+
+        global matches
+        if len(matches) < number:
+            return
+        mymatch: Region = matches[number - 1]
+        if extend:
+            pt = mymatch.b if region.a < region.b else mymatch.a
+            start = sels[0].a
+            sels.clear()
+            sels.add(Region(start, pt))
+        else:
+            sels.clear()
+            sels.add(mymatch.a)
+        buf.show(sels[0], True)
+        return
+
 
 class FindNextCharacterCommand(FindNextCharacterBaseCommand):
     def run(self, _, **kwargs: str) -> None:
-        self.view.settings().set(key="has_stored_char", value=True)
+        self.view.settings().set(key="has_stored_search", value=True)
+        self.view.settings().set(key="sneak_override_find_keybinding", value=True)
         self.view.settings().set(key="waiting_for_char", value=False)
         if bool(kwargs):
             mychar = kwargs['character']
@@ -829,22 +871,25 @@ class FindNextCharacterCommand(FindNextCharacterBaseCommand):
         character, forward, extend = char_forward_tuple
         search_string: str = character + mychar
         char_forward_tuple = (search_string, forward, extend)
-        self.move_or_expand_selections(search_string, forward, extend)
-        self.get_next_chars(forward, search_string)
+        self.execute(forward, search_string, extend)
+
 
 class FindNextCharacterListener(sublime_plugin.EventListener):
     def on_window_command(self, window: sublime.Window, _, __):
-        view: View | None = window.active_view()
+        view: Union[View,None] = window.active_view()
         if view is None:
             return
+        view.erase_phantoms("Sneak")
+        view.erase_regions("Sneak")
         view.settings().set(key="waiting_for_char", value=False)
-        view.settings().set(key="has_stored_char", value=False)
+        view.settings().set(key="has_stored_search", value=False)
 
     def on_text_command(self, view: View, command_name: str, _):
         view.erase_regions("Sneak")
+        view.erase_phantoms("Sneak")
         if (command_name != "find_next_character" and command_name != "repeat_find_next_character"
         and command_name != "store_character" and command_name != "revert_selection"):
-            view.settings().set(key="has_stored_char", value=False)
+            view.settings().set(key="has_stored_search", value=False)
             view.settings().set(key="waiting_for_char", value=False)
 
 
