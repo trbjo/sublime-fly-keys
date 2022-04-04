1ce4a97a616ec40d592b51357e6d244b3fa2dbac
Troels Bjørnskov <troels@bjoernskov.org>
2022-04-05 00:16:46 +0200

    Add fancy go to line

---
+++ enhanced_goto_line.py
@@ -0,0 +1,59 @@
+from sublime import View, Region, Selection
+import sublime_plugin
+from typing import Union
+
+should_extend: bool = False
+should_change_to_bol: bool = False
+first_view: Union[View,None] = None
+old_pos: int = -1
+
+class GotoInputListener(sublime_plugin.EventListener):
+    def on_query_context(self, view: View, key: str, _, __: bool, ___):
+        global should_extend
+        global should_change_to_bol
+        global first_view
+
+        # this line listens for the keys "sent" in the keymap file
+        if key =="can_expand":
+            first_view = view
+            if view.sel()[0].empty():
+                should_extend = False
+                should_change_to_bol = True
+            else:
+                global old_pos
+                old_pos = view.sel()[0].begin()
+                should_extend = True
+                should_change_to_bol = False
+            return True
+        else:
+            return False
+
+
+    def on_activated(self, view: View):
+        if view is None:
+            return
+        if view.element() is not None:
+            return
+        global first_view
+        if first_view is None:
+            return
+        if view.id() != first_view.id():
+            return
+        global should_extend
+        global should_change_to_bol
+        if should_extend:
+            should_extend = False
+            sels: Selection = view.sel()
+            new_pos = view.sel()[0].a
+            sels.clear()
+            if new_pos > old_pos:
+                view.sel().add(Region(view.line(old_pos).begin(),view.full_line(new_pos).end()))
+            else:
+                view.sel().add(Region(view.line(new_pos).begin(),view.full_line(old_pos).end()))
+        elif should_change_to_bol:
+            should_change_to_bol = False
+            next_res, _ = view.find(r'\S|^$|^\s+$', view.sel()[0].a)
+            view.sel().clear()
+            view.sel().add(next_res)
+
+        return
