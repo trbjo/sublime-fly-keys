8bc2308ce3919abd08ef505ca4c402e9bbf29a3a
Troels Bjørnskov <troels@bjoernskov.org>
2022-04-05 08:02:53 +0200

    Use enum instead of two bools

--- enhanced_goto_line.py
+++ enhanced_goto_line.py
@@ -1,36 +1,41 @@
 from sublime import View, Region, Selection
 import sublime_plugin
 from typing import Union
+from enum import IntEnum
 
-should_extend: bool = False
-should_change_to_bol: bool = False
+class Action(IntEnum):
+    DO_NOTHING = 0
+    CHANGE_TO_BOL = 1
+    EXTEND = 2
+
+action: Action = Action.DO_NOTHING
 first_view: Union[View,None] = None
 old_pos: int = -1
 
 class GotoInputListener(sublime_plugin.ViewEventListener):
     def on_query_context(self, key: str, _, __: bool, ___):
         view: View = self.view
-        global should_extend
-        global should_change_to_bol
+        global action
         global first_view
 
         # this line listens for the keys "sent" in the keymap file
         if key =="can_expand":
             first_view = view
             if view.sel()[0].empty():
-                should_extend = False
-                should_change_to_bol = True
+                action = Action.CHANGE_TO_BOL
             else:
                 global old_pos
                 old_pos = view.sel()[0].end()
-                should_extend = True
-                should_change_to_bol = False
+                action = Action.EXTEND
             return True
         else:
             return None
 
 
     def on_activated(self):
+        global action
+        if action == Action.DO_NOTHING:
+            return
         view: View = self.view
         if view is None:
             return
@@ -40,24 +45,23 @@ class GotoInputListener(sublime_plugin.ViewEventListener):
         if first_view is None:
             return
         if view.id() != first_view.id():
+            action = Action.DO_NOTHING
             return
         sels: Selection = view.sel()
         if sels[0].end() == old_pos:
+            action = Action.DO_NOTHING
             return
-        global should_extend
-        global should_change_to_bol
-        if should_extend:
-            should_extend = False
+        if action == Action.EXTEND:
             new_pos = view.sel()[0].a
             sels.clear()
             if new_pos > old_pos:
                 view.sel().add(Region(view.line(old_pos).begin(),view.full_line(new_pos).end()))
             else:
                 view.sel().add(Region(view.line(new_pos).begin(),view.full_line(old_pos).end()))
-        elif should_change_to_bol:
-            should_change_to_bol = False
+        elif action == Action.CHANGE_TO_BOL:
             next_res, _ = view.find(r'\S|^$|^\s+$', view.sel()[0].a)
             view.sel().clear()
             view.sel().add(next_res)
 
+        action = Action.DO_NOTHING
         return
