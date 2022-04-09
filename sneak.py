8f07b8e1a54d2828c0614938333be5048ce3522f
Troels Bjørnskov <troels@bjoernskov.org>
2022-04-09 23:25:35 +0200

    Sneak: Improve multi selection performance

--- sneak.py
+++ sneak.py
@@ -1,116 +1,182 @@
 from sublime import LAYOUT_INLINE, View, Region, Selection
 import sublime
 import sublime_plugin
-
 from typing import List, Tuple, Union
 
 # tuple of (char: str, forward: bool, extend: bool)
 char_forward_tuple: Tuple[str, bool, bool] = ('', True, False)
 matches : List[Region] = []
+myhtml="""<body id="my-plugin-feature"><style>div.matcher{{padding: 0 2px 0 1px; margin: 0; border-radius:2px;background-color:{background};color:{color};}}</style><div class="matcher">{counter}</div></body>"""
+charlist = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
 class FindNextCharacterBaseCommand(sublime_plugin.TextCommand):
-    def find_next(self, forward: bool, char: str, pt: int, num_matches: int) -> List[int]:
-        buf = self.view
-        pts: List[int] = []
-        if forward:
-            mybuf = self.view.substr(Region(pt+1,buf.size()))
-        else:
-            mybuf = self.view.substr(Region(0, pt-1))[::-1]
-            char = char[::-1]
-        new_pt: int = 0
-        for _ in range(num_matches):
-            if forward:
-                try:
-                    new_pt = mybuf.index(char, new_pt) + 1
-                    pts.append(pt + new_pt)
-                except ValueError:
-                    return pts
-            else:
-                try:
-                    new_pt = mybuf.index(char, new_pt) + 2
-                    pts.append(pt - new_pt)
-                except ValueError:
-                    return pts
-        return pts
-
-
-    myhtml="""<body id="my-plugin-feature"><style>div.matcher{{padding: 0 2px 0 1px; margin: 0; border-radius:2px;background-color:{background};color:{color};}}</style><div class="matcher">{counter}</div></body>"""
-    charlist = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    def execute(self, forward: bool, search_string: str, extend: bool) -> None:
-        buf = self.view
-        sels: Selection = buf.sel()
-        regs_to_add: List[Union[Region,int]] = []
-        regs_to_subtract: List[Region] = []
-        offset = 1 if not forward and len(search_string) == 1 else 0
-        for region in sels:
-            pts = self.find_next(forward, search_string, region.b, 1)
-            if not pts:
-                return
-            pt = pts[0]
-            if region.a == region.b:
-                if extend:
-                    tp = pt + len(search_string) if forward else pt -1
-                    regs_to_add.append(sublime.Region(region.a, tp))
-                else:
-                    tp = pt if forward else pt -1
-                    regs_to_subtract.append(region)
-                    regs_to_add.append(tp + offset)
-                # normal sel
-            elif region.a < region.b:
-                if forward:
-                    regs_to_add.append(Region(region.b -1, pt + len(search_string)))
-                else:
-                    if pt < region.a:
-                        regs_to_subtract.append(Region(region.b, region.a))
-                        regs_to_add.append(Region(region.a))
-                    else:
-                        regs_to_subtract.append(Region(region.b, pt+1))
-                # reverse sel
-            elif region.a > region.b:
-                if forward:
-                    if pt > region.a:
-                        regs_to_subtract.append(Region(region.a, region.b))
-                        regs_to_add.append(Region(region.a))
-                    else:
-                        regs_to_subtract.append(Region(region.b, pt))
-                else:
-                    regs_to_add.append(Region(region.a, pt -1 + offset))
-
-        for reg in regs_to_subtract:
-            sels.subtract(reg)
-        sels.add_all(regs_to_add)
-        buf.show(buf.sel()[-1], True)
-
-        # now we look for highlights
+    def set_single(self, forward: bool, extend: bool, search_string: str) -> None:
+        view = self.view
+        region = view.sel()[0]
         global matches
         matches = []
-        offset = offset-1 if not forward else offset
-        off_length = offset+len(search_string)
-        # we use phantoms if we have one match, regions, if more
-        if len(sels) > 1:
-            new_offset = 1 if not forward and len(search_string) == 2 else 0
-            light_hl: List[Region] = []
-            for i,region in enumerate(sels):
-                new_pt = self.find_next(forward, search_string, region.b, 1)[0]
-                try:
-                    if new_pt == sels[i+1 if forward else i-1].b + new_offset:
-                        light_hl.append(Region(new_pt+offset, new_pt+off_length))
+        bg_color = view.style()["accent"]
+        fg_color = view.style()["background"]
+        try:
+            if forward:
+                mybuf = self.view.substr(Region(region.b+1,view.size()))
+                new_pt: int = mybuf.index(search_string) + 1
+                pt = region.b + new_pt
+                if region.a == region.b:
+                    if extend:
+                        view.sel().add(sublime.Region(region.a, pt + len(search_string)))
                     else:
-                        matches.append(Region(new_pt+offset, new_pt+off_length))
-                except IndexError:
-                    matches.append(Region(new_pt+offset, new_pt+off_length))
-                    break
-            if matches:
-                self.view.add_regions("Sneak", matches, "accent", flags=sublime.DRAW_NO_OUTLINE)
-                matches = []
+                        view.sel().clear()
+                        view.sel().add(pt)
+                elif region.a < region.b:
+                    view.sel().add(Region(region.b -1, pt + len(search_string)))
+                elif region.a > region.b:
+                    if pt > region.a:
+                        view.sel().subtract(Region(region.a, region.b))
+                        view.sel().add(region.a)
+                    else:
+                        view.sel().subtract(Region(region.b, pt))
+                view.show(view.sel()[0], True)
+
+                for i in range(10):
+                    new_pt = mybuf.index(search_string, new_pt) + 1
+                    mypt = region.b + new_pt
+                    myreg: Region = Region(mypt, mypt+len(search_string))
+                    matches.append(myreg)
+                    self.view.add_phantom("Sneak", region=myreg, content=myhtml.format(counter=charlist[i], background=bg_color, color=fg_color), layout=LAYOUT_INLINE)
+            else:
+                offset = 1 if len(search_string) == 1 else 0
+                mybuf = self.view.substr(Region(0, region.b-1))[::-1]
+                char = search_string[::-1]
+                new_pt = mybuf.index(char) + 2
+                pt = region.b - new_pt
+
+                if region.a == region.b:
+                    if extend:
+                        view.sel().add(sublime.Region(region.a, pt -1 + offset))
+                    else:
+                        view.sel().clear()
+                        view.sel().add(pt-1+offset)
+                elif region.a < region.b:
+                    if pt < region.a:
+                        view.sel().subtract(Region(region.b, region.a))
+                        view.sel().add(region.a)
+                    else:
+                        view.sel().subtract(Region(region.b, pt+1))
+                elif region.a > region.b:
+                    view.sel().add(Region(region.a, pt -1 + offset))
+                view.show(view.sel()[0], True)
+
+                for i in range(10):
+                    new_pt = mybuf.index(char, new_pt) + 2
+                    mypt = region.b - new_pt + offset - 1
+                    myreg: Region = Region(mypt, mypt+len(search_string))
+                    matches.append(myreg)
+                    self.view.add_phantom("Sneak", region=myreg, content=myhtml.format(counter=charlist[i], background=bg_color, color=fg_color), layout=LAYOUT_INLINE)
+        except ValueError:
+            return
+
+
+    def set_multiple(self, forward: bool, extend: bool, search_string: str) -> None:
+        view = self.view
+        regs_to_add: List[Union[Region,int]] = []
+        regs_to_subtract: List[Region] = []
+        offset = 0
+
+        light_hl: List[Region] = []
+        regular_hl: List[Region] = []
+        try:
+            if forward:
+                first_reg = view.sel()[0].b
+                mybuf = self.view.substr(Region(first_reg+1,view.size()))
+                for region in view.sel():
+                    new_pt: int = mybuf.index(search_string, region.b - first_reg +1) + 1 + first_reg
+                    if region.a == region.b:
+                        if extend:
+                            regs_to_add.append(sublime.Region(region.a, new_pt + len(search_string)))
+                        else:
+                            regs_to_subtract.append(region)
+                            regs_to_add.append(new_pt)
+                    elif region.a < region.b:
+                        regs_to_add.append(Region(region.b -1, new_pt + len(search_string)))
+                    elif region.a > region.b:
+                        if new_pt > region.a:
+                            regs_to_subtract.append(Region(region.a, region.b))
+                            regs_to_add.append(region.a)
+                        else:
+                            regs_to_subtract.append(Region(region.b, new_pt))
+                for reg in regs_to_subtract:
+                    view.sel().subtract(reg)
+                view.sel().add_all(regs_to_add)
+                view.show(view.sel()[-1], True)
+
+                offset = offset-1 if not forward else offset
+                off_length = offset+len(search_string)
+                for i,region in enumerate(view.sel()):
+                    new_pt: int = mybuf.index(search_string, region.b - first_reg + 1) + first_reg + 1
+                    try:
+                        if new_pt == view.sel()[i+1].b:
+                            light_hl.append(Region(new_pt+offset, new_pt+off_length))
+                        else:
+                            regular_hl.append(Region(new_pt+offset, new_pt+off_length))
+                    except IndexError:
+                        regular_hl.append(Region(new_pt+offset, new_pt+off_length))
+                        break
+            else:
+                offset = 1 if len(search_string) == 1 else 0
+                last_reg = view.sel()[-1].b
+                mybuf = self.view.substr(Region(0, last_reg-1))[::-1]
+                char = search_string[::-1]
+                for region in view.sel():
+                    pt: int =last_reg - mybuf.index(char, last_reg - region.b) - 2
+                    if region.a == region.b:
+                        if extend:
+                            regs_to_add.append(sublime.Region(region.a, pt -1 + offset))
+                        else:
+                            regs_to_subtract.append(region)
+                            regs_to_add.append(pt-1+offset)
+                    elif region.a < region.b:
+                        if pt < region.a:
+                            regs_to_subtract.append(Region(region.b, region.a))
+                            regs_to_add.append(region.a)
+                        else:
+                            regs_to_subtract.append(Region(region.b, pt+1))
+                    elif region.a > region.b:
+                        regs_to_add.append(Region(region.a, pt -1 + offset))
+                    view.show(view.sel()[0], True)
+
+                for reg in regs_to_subtract:
+                    view.sel().subtract(reg)
+                view.sel().add_all(regs_to_add)
+                view.show(view.sel()[-1], True)
+
+                offset = offset-1 if not forward else offset
+                off_length = offset+len(search_string)
+                new_offset = 1 if not forward and len(search_string) == 2 else 0
+                for i,region in enumerate(view.sel()):
+                    pt: int = last_reg - mybuf.index(char, last_reg - region.b) - 2
+                    try:
+                        if pt == view.sel()[i-1].b + new_offset:
+                            light_hl.append(Region(pt+offset, pt+off_length))
+                        else:
+                            regular_hl.append(Region(pt+offset, pt+off_length))
+                    except IndexError:
+                        regular_hl.append(Region(pt+offset, pt+off_length))
+                        break
+
+            if regular_hl:
+                self.view.add_regions("Sneak", regular_hl, "accent", flags=sublime.DRAW_NO_OUTLINE)
             if light_hl:
                 self.view.add_regions("Sneaks", light_hl, "light", flags=sublime.DRAW_NO_OUTLINE)
-        else:
-            bg_color = buf.style()["accent"]
-            fg_color = buf.style()["background"]
-            matches = [Region(pt+offset, pt+off_length) for pt in self.find_next(forward, search_string, sels[0].b, 10)]
-            for i,match in enumerate(matches):
-                self.view.add_phantom("Sneak", region=match, content=self.myhtml.format(counter=self.charlist[i], background=bg_color, color=fg_color), layout=LAYOUT_INLINE)
 
+        except IndexError:
+            return
+
+    def execute(self, forward: bool, search_string: str, extend: bool) -> None:
+        if len(self.view.sel()) == 1:
+            self.set_single(forward, extend, search_string)
+        else:
+            self.set_multiple(forward, extend, search_string)
 
 class StoreCharacterCommand(FindNextCharacterBaseCommand):
     def run(self, _, forward: bool, character: str='', extend: bool=False) -> None:
@@ -134,8 +200,8 @@ class GoToNthMatchCommand(FindNextCharacterBaseCommand):
             number: int = kwargs['number']
         else:
             return
-        buf = self.view
-        sels: Selection = buf.sel()
+        view = self.view
+        sels: Selection = view.sel()
         region = sels[0]
         if len(sels) != 1:
             return
@@ -154,7 +220,7 @@ class GoToNthMatchCommand(FindNextCharacterBaseCommand):
         else:
             sels.clear()
             sels.add(mymatch.a)
-        buf.show(sels[0], True)
+        view.show(sels[0], True)
         return
 
 class FindNextCharacterCommand(FindNextCharacterBaseCommand):
