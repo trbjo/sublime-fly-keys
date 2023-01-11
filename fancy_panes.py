fe5e0810e4c59b18b16ddff6ecc5ea7923a1e46f
Troels Bjørnskov <troels@bjoernskov.org>
2023-01-11 12:36:04 +0100

    more cleanup

--- fancy_panes.py
+++ fancy_panes.py
@@ -1,20 +1,18 @@
-from typing import Dict, List, Set, Tuple, Union
-
 import sublime
 from sublime import Region, View, active_window
-from sublime_plugin import TextCommand, ViewEventListener, WindowCommand
+from sublime_plugin import WindowCommand
 
 
 class FancyOpenPaneCommand(WindowCommand):
     def run(self) -> None:
-
-        orig_view: Union[View, None] = active_window().active_view()
-        active_window().run_command("clone_file")
-        active_window().run_command("new_pane")
+        w = active_window()
+        orig_view = w.active_view()
+        w.run_command("clone_file")
+        w.run_command("new_pane")
         if orig_view is None:
             return
         carets = orig_view.sel()
-        new_view: View = active_window().active_view()
+        new_view: View = w.active_view()
         new_view.sel().clear()
         new_view.sel().add_all(carets)
         new_view.show_at_center(carets[0].b)
@@ -22,22 +20,21 @@ class FancyOpenPaneCommand(WindowCommand):
 
 class FancyClosePaneCommand(WindowCommand):
     def run(self) -> None:
-
-        if active_window().num_groups() < 2:
+        w = active_window()
+        if w.num_groups() < 2:
             return
 
-        active_view = active_window().active_view()
+        active_view = w.active_view()
         if active_view is None:
-            active_window().run_command("close_pane")
             return
 
         if active_view.is_dirty():
             active_view.set_scratch(True)
 
-        active_window().run_command("close_pane")
+        w.run_command("close_pane")
 
         buffers = {active_view.buffer_id()}
-        for v in active_window().views_in_group(active_window().active_group()):
+        for v in w.views_in_group(w.active_group()):
             if active_view.id() != v.id() and v.buffer_id() in buffers:
                 v.close()
             else:
