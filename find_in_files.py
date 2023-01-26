b29955182893ad85de140f31796dc3cc7a0a2be2
Troels Bjørnskov <troels@bjoernskov.org>
2023-01-26 23:21:39 +0100

    improve find_in_files

--- find_in_files.py
+++ find_in_files.py
@@ -3,7 +3,7 @@ from os import getenv, path
 from typing import Union
 
 import sublime
-from sublime import Region, View, active_window
+from sublime import NewFileFlags, Region, View, active_window
 from sublime_plugin import EventListener, TextCommand, WindowCommand
 
 from .navigate_paragraphs import build_or_rebuild_ws_for_buffer
@@ -41,35 +41,43 @@ class FocusPanelCommand(WindowCommand):
             else:
                 view.settings().set("color_scheme", "Light_dim.sublime-color-scheme")
             self.window.focus_view(view)
-            hej = view.viewport_position()
-            vp = (400.0, 500.0)
-            view.set_viewport_position(vp)
-            print(f"{hej=}")
 
 
 class FindInFilesGotoCommand(TextCommand):
-    def run(self, _) -> None:
-        view = self.view
-        if view is None:
+    def run(self, _, preview=False) -> None:
+
+        if (view := self.view) is None:
             return
-        window: Union[sublime.Window, None] = view.window()
-        if window is None:
+
+        if (window := view.window()) is None:
             return
+
+        params = sublime.ENCODED_POSITION
+        if preview:
+            params += NewFileFlags.TRANSIENT
+
         line_no = self.get_line_no()
         file_name = self.get_file()
+
         if line_no is not None and file_name is not None:
             caretpos = view.sel()[0].begin()
             (_, col) = view.rowcol(caretpos)
             file_loc = "%s:%s:%s" % (file_name, line_no, col - 6)
-            new_view = window.open_file(file_loc, sublime.ENCODED_POSITION)
+            window.open_file(file_loc, params)  # type: ignore
         elif file_name is not None:
-            new_view = window.open_file(file_name)
+            file_loc = "%s:%s:%s" % (file_name, 1, 1)
+            window.open_file(file_loc, params)  # type: ignore
+        else:
+            return None
+
+        if not preview:
+            self.view.window().run_command("hide_panel", {"cancel": True})
 
     def get_line_no(self):
         view = self.view
         if len(view.sel()) == 1:
             line_text = view.substr(view.line(view.sel()[0]))
-            match = re.match(r"\s*(\d+).+", line_text)
+            match = re.match(r"\s*(\d+):.+", line_text)
             if match:
                 return match.group(1)
         return None
@@ -78,6 +86,10 @@ class FindInFilesGotoCommand(TextCommand):
         view = self.view
         if len(view.sel()) == 1:
             line = view.line(view.sel()[0])
+            if line.empty() or re.match(
+                r"\d+ matches across \d+ files", view.substr(line)
+            ):
+                return None
             while line.begin() > 0:
                 line_text = view.substr(line)
                 match = re.match(r"(.+):$", line_text)
