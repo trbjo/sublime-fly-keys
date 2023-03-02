8104b2735b0974b9b7df39780d2a8a46a6539238
Troels Bjørnskov <troels@bjoernskov.org>
2023-03-02 09:11:52 +0100

    refactor: simplify listeners

--- listeners.py
+++ listeners.py
@@ -4,9 +4,9 @@ from typing import Optional, Union
 import sublime
 import sublime_plugin
 from sublime import Selection, View, active_window
-from sublime_api import view_full_line_from_point as full_line
-from sublime_api import view_line_from_point as line
-from sublime_api import view_selection_add_region as add_region
+from sublime_api import view_full_line_from_point as full_line  # pyright: ignore
+from sublime_api import view_line_from_point as line  # pyright: ignore
+from sublime_api import view_selection_add_region as add_region  # pyright: ignore
 
 from .base import build_or_rebuild_ws_for_buffer, interesting_regions, maybe_rebuild
 
@@ -33,15 +33,50 @@ revert_to_normal_mode = [
 ]
 
 
-class BlockCursorOnStartupListener(sublime_plugin.EventListener):
+def pre_command(v: Optional[View], command_name):
+    if v is None:
+        return
+
+    if v.element() is not None:
+        return
+
+    v.erase_phantoms("Sneak")
+    v.erase_regions("Sneak")
+    v.erase_regions("Sneaks")
+
+    if command_name not in revert_to_normal_mode:
+        v.settings().set(key="has_stored_search", value=False)
+        v.settings().set(key="needs_char", value=False)
+
+
+class WindowListener(sublime_plugin.EventListener):
+    def on_window_command(self, window: sublime.Window, command_name, _):
+        pre_command(window.active_view(), command_name)
+
     def on_init(self, views):
         for v in views:
-            command_mode = v.settings().get(key="command_mode")
-            if command_mode:
+            if v.element() is not None:
+                continue
+
+            if v.settings().get(key="command_mode"):
                 v.settings().set(key="block_caret", value=True)
 
+            global interesting_regions
+            if v.buffer_id() not in interesting_regions:
+                build_or_rebuild_ws_for_buffer(v, now=True)
 
-class ModifiedViewListener(sublime_plugin.ViewEventListener):
+
+class BufferListener(sublime_plugin.ViewEventListener):
+    # thanks to Odatnurd from Sublime Discord
+    # useful for relative line numbers, build_or_rebuild_ws_for_buffer
+    @classmethod
+    def applies_to_primary_view_only(cls) -> bool:
+        """
+        :returns: Whether this listener should apply only to the primary view
+                  for a file or all of its clones as well.
+        """
+        return False
+
     def on_modified_async(self):
         view: View = self.view
         if view.element() is None:
@@ -56,44 +91,62 @@ class ModifiedViewListener(sublime_plugin.ViewEventListener):
         if view.buffer_id() not in interesting_regions and view.element() is None:
             build_or_rebuild_ws_for_buffer(view, now=True)
 
-
-class GotoInputListener(sublime_plugin.ViewEventListener):
-    def on_query_context(self, key: str, _, __: bool, ___):
-        if key != "can_expand":
-            return None
+    def on_query_context(self, key: str, operator, operand: bool, match_all: bool):
         view: View = self.view
-        for v in active_window().views():
-            v.settings().set("relative_line_numbers", False)
-        global first_view
-        global action
-        first_view = view
-        global old_pos
-        if view.sel()[0].empty():
-            old_pos = view.sel()[0].b
-            action = Action.CHANGE_TO_BOL
-        else:
-            old_pos = view.sel()[0].a
-            action = Action.EXTEND
-        return True
+        if view.element() is not None:
+            return
+
+        if key == "side_bar_visible":
+            return active_window().is_sidebar_visible() == operand
+
+        if key == "num_groups":
+            return active_window().num_groups() == operand
+
+        if key == "reversed_selection":
+            if match_all:
+                rhs = all([r.a > r.b for r in view.sel()])
+            else:
+                rhs = view.sel()[0].a > view.sel()[0].b
+            return operator == sublime.OP_EQUAL and rhs == operand
+
+        if key == "can_expand":
+            for v in active_window().views():
+                v.settings().set("relative_line_numbers", False)
+            global first_view
+            global action
+            first_view = view
+            global old_pos
+            if view.sel()[0].empty():
+                old_pos = view.sel()[0].b
+                action = Action.CHANGE_TO_BOL
+            else:
+                old_pos = view.sel()[0].a
+                action = Action.EXTEND
+            return True
 
     def on_activated(self):
         view: View = self.view
-        for v in active_window().views():
-            v.settings().set("relative_line_numbers", True)
+        if view.element() is not None:
+            return
+
         global action
         if action == Action.DO_NOTHING:
             return
-        if view.element() is not None:
-            return
+
+        for v in active_window().views():
+            v.settings().set("relative_line_numbers", True)
+
         global first_view
         if first_view is None:
             return
+
         sels: Selection = view.sel()
         if sels[0].end() == old_pos:
             action = Action.DO_NOTHING
             return
 
         vid = view.id()
+
         if action == Action.EXTEND and vid == first_view.id():
             new_pos = view.sel()[0].b
             if new_pos > old_pos:
@@ -111,26 +164,10 @@ class GotoInputListener(sublime_plugin.ViewEventListener):
 
         action = Action.DO_NOTHING
 
-
-class EventListener(sublime_plugin.EventListener):
-    def on_query_context(self, view, key, operator, operand, match_all):
-        if key == "side_bar_visible":
-            return active_window().is_sidebar_visible() == operand
-
-        if key == "reversed_selection":
-            if match_all:
-                rhs = all([r.a > r.b for r in view.sel()])
-            else:
-                rhs = view.sel()[0].a > view.sel()[0].b
-            return operator == sublime.OP_EQUAL and rhs == operand
-
-        if key == "num_groups":
-            return active_window().num_groups() == 1
-
-
-class NextCharacterTextListener(sublime_plugin.ViewEventListener):
     def on_text_command(self, command_name: str, args):
         v = self.view
+        if v.element() is not None:
+            return
 
         if command_name != "set_number" and (
             multiplier := v.settings().get("multiplier")
@@ -140,33 +177,3 @@ class NextCharacterTextListener(sublime_plugin.ViewEventListener):
             v.settings().erase("multiplier")
             v.settings().erase("set_number")
         pre_command(v, command_name)
-
-    # thanks to Odatnurd from Sublime Discord
-    @classmethod
-    def applies_to_primary_view_only(cls) -> bool:
-        """
-        :returns: Whether this listener should apply only to the primary view
-                  for a file or all of its clones as well.
-        """
-        return False
-
-
-class NextCharacterWindowListener(sublime_plugin.EventListener):
-    def on_window_command(self, window: sublime.Window, command_name, _):
-        pre_command(window.active_view(), command_name)
-
-
-def pre_command(v: Optional[View], command_name):
-    if v is None:
-        return
-
-    if v.element() is not None:
-        return
-
-    v.erase_phantoms("Sneak")
-    v.erase_regions("Sneak")
-    v.erase_regions("Sneaks")
-
-    if command_name not in revert_to_normal_mode:
-        v.settings().set(key="has_stored_search", value=False)
-        v.settings().set(key="needs_char", value=False)
